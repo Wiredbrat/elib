ebd01ebc3eb374f0f33d9c0a53291e965c940982
wiredbrat <vermaatul291002@gmail.com>
2025-07-23 00:02:15 +0530

    install dependencies and minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import './App.css'
-
+import { ThemeContext } from './context/ThemeContext'
+import LoginRegisterForm from './components/LoginRegisterForm'
 function App() {
   const [count, setCount] = useState(0)
-
+  const {theme, setTheme} = useContext(ThemeContext)
   return (
     <>
-
+      <LoginRegisterForm/>
     </>
   )
 }
