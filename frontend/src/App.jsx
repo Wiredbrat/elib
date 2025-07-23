5e7188d27e54ab6dcf115a89fa796fad8572f39b
wiredbrat <vermaatul291002@gmail.com>
2025-07-23 23:55:49 +0530

    setup process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import { useContext, useState } from 'react'
 import './App.css'
 import { ThemeContext } from './context/ThemeContext'
 import LoginRegisterForm from './components/LoginRegisterForm'
+
 function App() {
   const [count, setCount] = useState(0)
   const {theme, setTheme} = useContext(ThemeContext)
   return (
     <>
-      <LoginRegisterForm/>
+      <div className='font-inter'>
+
+        <LoginRegisterForm/>
+      </div>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
-
+import { RouterProvider } from 'react-router-dom'
+import {router} from './layout/Layout.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <App />
+        <RouterProvider router={router}/>
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
