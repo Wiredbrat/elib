b98ba3f86169b6f2c0daeb9395b7a60e6f760a7a
wiredbrat <vermaatul291002@gmail.com>
2025-07-23 23:55:09 +0530

    define api routes to access

--- frontend/src/api_routes/user.routes.js
+++ frontend/src/api_routes/user.routes.js
@@ -1,7 +1,7 @@
 const userRoutes = {
   login: 'http://localhost:8000/api/v1/user/login',
   register: 'http://localhost:8000/api/v1/user/register',
-
+  otp: 'http://localhost:8000/api/v1/auth/send-otp'
 }
 
 export default userRoutes
