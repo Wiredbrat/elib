b14b78e2732777150708d40d1361a9f9f5b22694
wiredbrat <vermaatul291002@gmail.com>
2025-08-01 00:34:00 +0530

    add more api routes in route file

---
+++ frontend/src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const userRoutes = {
+  login: 'http://localhost:8000/api/v1/user/login',
+  register: 'http://localhost:8000/api/v1/user/register',
+  otp: 'http://localhost:8000/api/v1/auth/send-otp',
+  verifyOtp: 'http://localhost:8000/api/v1/auth/verify-otp'
+}
+
+export default userRoutes
