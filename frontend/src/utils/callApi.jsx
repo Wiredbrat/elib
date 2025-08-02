8d7e7317edbe37532b8059ec6cdf71647aabc160
wiredbrat <vermaatul291002@gmail.com>
2025-08-03 00:41:54 +0530

    add callApi function for handling api calling

---
+++ frontend/src/utils/callApi.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+export default async function callApi(url, reqType = 'get', data = null) {
+  // const [response, setResponse] = useState()
+
+  try {
+    let response =
+    reqType === 'post' 
+    ?await axios[reqType](url, data) 
+    :await axios[reqType](url)
+
+    console.log(response)
+    return { response } 
+
+  } catch (error) {
+    console.log({
+      StatusCode: 500,
+      data: error.data,
+      success: false 
+    })
+    return {
+      StatusCode: 500,
+      data: error.data,
+      success: false 
+    }
+  }
+  
+}
