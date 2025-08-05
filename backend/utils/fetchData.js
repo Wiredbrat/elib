46edc48149b0a3f5fa1fd5ecbff29ca5e8bbea2b
wiredbrat <vermaatul291002@gmail.com>
2025-08-05 23:19:18 +0530

    add limit to search data

--- backend/utils/fetchData.js
+++ backend/utils/fetchData.js
@@ -4,11 +4,12 @@ import ApiError from "./errorHandler.js";
 import ApiResponse from "./responseHandler.js";
 
 const fetchDataFromAPI = asyncHandler(async(req, res, next) => {
-    const { title } = req.query
-    if(!title) {
+    const { title, limit } = req.query
+    console.log(title)
+    if(!title || !limit) {
       throw new ApiError(401, 'enter book name')
     }
-    const data = await axios.get(`https://openlibrary.org/search.json?title=${title}`)
+    const data = await axios.get(`https://openlibrary.org/search.json?title=${title}&limit=${limit}`)
     const newData = data.data
     console.log(newData)
     // req.bookData = newData.docs
