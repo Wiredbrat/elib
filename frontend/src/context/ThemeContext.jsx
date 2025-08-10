4e5b8a8fcf7ec07f3198f410f17b74ad8479125d
wiredbrat <vermaatul291002@gmail.com>
2025-08-11 00:11:13 +0530

    update theme access model

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -5,16 +5,19 @@ const ThemeContext = createContext(null)
 const ThemeProvider = ({children}) => {
   
   const [theme, setTheme] = useState('light')
-   
+  const html = document.documentElement
   const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
   if(systemTheme) {
     localStorage.setItem('theme', 'dark')
+    theme === 'dark' ? html.setAttribute('data-theme', 'dark') :  html.removeAttribute('data-theme')
   }else {
     localStorage.setItem('theme', 'light')
+     html.removeAttribute('data-theme')
   }
   
   useEffect(() => {
     const currTheme = localStorage.getItem('theme')
+    localStorage.setItem('theme', currTheme)
     setTheme(currTheme)
   }, [])
   
