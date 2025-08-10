0b789794ccf71adb200d941b8f5e1080e4d20710
wiredbrat <vermaatul291002@gmail.com>
2025-08-10 20:28:15 +0530

    fix bugs and add more functionality

--- frontend/src/components/LoginRegisterForm.jsx
+++ frontend/src/components/LoginRegisterForm.jsx
@@ -55,7 +55,7 @@ const LoginRegisterForm = () => {
       console.log(response)
       if(response.data?.statusCode === 200) {
         toast.success('Welcome', {position: 'bottom-center'})
-        navigate('/dashboard', {replace: true})
+        navigate('/discover', {replace: true})
         return
       }else{
         toast.error('Invalid Credentials!', {position: 'bottom-center'})
@@ -83,13 +83,13 @@ const LoginRegisterForm = () => {
     <>
       <div className='h-screen w-full flex flex-col relative items-center overflow-hidden py-4'>
         <div className='flex justify-end items-center gap-2 text-sm'>
-          <p className='text-gray-600'>{isRegister? 'Already Registered?': `Don't have an account?`}</p>
+          <p className='text-color'>{isRegister? 'Already Registered?': `Don't have an account?`}</p>
           
           <motion.button
             initial={{opacity:0, scale: 0}}
             animate={{opacity: 1, scale:1, translate: {duration: 1}}} 
             onClick={() => setIsRegister((prev) => !prev)}
-            className='text-black bg-gray-200 font-sans3 px-2 py-1 rounded-md shadow'
+            className='text-color button font-sans3 px-2 py-1 rounded-md shadow'
           >
             {isRegister? 'Sign Up' : 'Sign In'}
         </motion.button>
@@ -102,11 +102,11 @@ const LoginRegisterForm = () => {
               animate={{opacity:1, x: 0, scale: 1}}
               exit={{opacity: 0, x: -300, scale: 1}}
               key='registrationForm'
-              className='p-4 w-full absolute top-0'
+              className='p-4 w-full absolute top-0 text-color'
             >
 
               <p className='text-center my-6'>
-                <span className='inline-block font-bold text-xl text-transparent bg-linear-to-r from-black to-gray-300 bg-clip-text'>
+                <span className='inline-block font-bold text-xl text-transparent text-color'>
                   Sign in to 
                 </span> &nbsp;
                 <span className='inline-block font-bold text-xl text-transparent bg-linear-to-r from-blue-500 to-blue-300 bg-clip-text'>
@@ -114,27 +114,27 @@ const LoginRegisterForm = () => {
                 </span>
               </p>
               <form onSubmit={handleSubmit(onSubmitRegistrationForm)} 
-                className='flex flex-col gap-4 text-sm'
+                className='flex flex-col gap-4 text-sm '
               >
                 <input 
                   type="text" {...register("username", { required: true })} 
                   placeholder='User Name'
-                  className='border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
+                  className='input border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
                 />
 
                 <input 
                   type="text" {...register("fullName", { required: true })} 
                   placeholder='Full Name'
-                  className='border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
+                  className='input border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
                 />
 
                 <input 
                   type="text" {...register("email", { required: true })} 
                   placeholder='Email'
-                  className='border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
+                  className='input border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
                 />
 
-                <div className='flex border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'>
+                <div className='input flex border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'>
                   <input 
                     type={`${showPassword ? 'text': 'password'}`} {...register("password", { required: true, minLength:{value: 6, message: 'password is too short'}})} 
                     onCopy={(e) => e.preventDefault()}
@@ -153,7 +153,7 @@ const LoginRegisterForm = () => {
                 
                   <button
                     type="submit" value="Register" 
-                    className='bg-[var(--button)] text-[var(--primary)] rounded-md py-3 shadow hover:scale-[1.01] duration-150 active:scale-[0.99] cursor-pointer'
+                    className='button rounded-md py-3 shadow hover:scale-[1.01] duration-150 active:scale-[0.99] cursor-pointer'
                   >
                     Register
                   </button>
@@ -166,10 +166,10 @@ const LoginRegisterForm = () => {
               animate={{opacity:1,  x: 0, scale: 1}}
               exit={{opacity: 0,  x: 300, scale: 1}}
               key='loginForm'
-              className='p-4 w-full absolute top-0'
+              className='p-4 w-full absolute top-0 text-color'
             >
               <p className='text-center my-6'>
-                <span className='inline-block font-bold text-xl text-transparent bg-linear-to-r from-black to-gray-300 bg-clip-text'>
+                <span className='inline-block font-bold text-xl text-transparent bg-linear-to-r text-color bg-clip-text'>
                   Sign Up to 
                 </span> &nbsp;
                 <span className='inline-block font-bold text-xl text-transparent bg-linear-to-r from-blue-500 to-blue-300 bg-clip-text '>
@@ -184,31 +184,31 @@ const LoginRegisterForm = () => {
                   type="text" {...register("usernameOrEmail", { required: true })} 
                   aria-invalid={errors.usernameOrEmail ? 'true' : 'false'}
                   placeholder='Username or Email'
-                  className='border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
+                  className='input border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow'
                 />
                 
 
-                <div className='flex border border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow select-none selection:bgt'>
+                <div className='flex input border-gray-200 w-full px-3 py-4 rounded-xl outline-0 shadow select-none selection:bgt'>
                   <input 
                     onCopy={(e) => e.preventDefault()}
                     onCut={(e) => e.preventDefault()}
                     onSelect={(e) => e.preventDefault()}
                     type={`${showPassword ? 'text': 'password'}`} {...register("password", { required: true})} 
                     placeholder='Password'
-                    className='outline-0 border-0 w-[90%] select-none'
+                    className='outline-0 border-0 w-[90%] select-none '
                   /> 
                   <span 
                     onClick={() => {setShowPassword((prev => !prev))}}
                     className='cursor-pointer'
                   >
-                      {showPassword? <i className="fa-solid fa-eye-slash text-gray-500"></i> : <i className="fa-solid fa-eye text-gray-500"></i>}
+                      {showPassword? <i className="fa-solid fa-eye-slash text-color"></i> : <i className="fa-solid fa-eye text-color"></i>}
                   </span>
                 </div>
                 
                   <input 
                   type="submit" 
                   value="Login"
-                  className='bg-[var(--button)] text-[var(--primary)] rounded-md py-3 shadow hover:scale-[1.01] duration-150 active:scale-[0.99] cursor-pointer'
+                  className='button rounded-md py-3 shadow hover:scale-[1.01] duration-150 active:scale-[0.99] cursor-pointer'
                   />
                 
               </form>
