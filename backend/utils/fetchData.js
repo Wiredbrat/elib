b7367b514bfa6a8afe0568681193a81b3e6f7cd3
wiredbrat <vermaatul291002@gmail.com>
2025-08-12 00:18:12 +0530

    define response to send

--- backend/utils/fetchData.js
+++ backend/utils/fetchData.js
@@ -9,16 +9,37 @@ const fetchDataFromAPI = asyncHandler(async(req, res, next) => {
     if(!title || !limit) {
       throw new ApiError(401, 'enter book name')
     }
-    const data = await axios.get(`https://openlibrary.org/search.json?title=${title}&limit=${limit}`)
-    const newData = data.data
-    console.log(newData)
-    // req.bookData = newData.docs
-    // next()
 
-    // this is response for checking 
+    const data = await axios.get(`https://openlibrary.org/search.json?title=${title}&limit=${limit}`)
+    console.log(data.data)
+    if(!data.data.docs.length) {
+      return new ApiError(404, 'no book found')
+    }
+
+    const booksData = data.data.docs?.map((book) => {
+      const coverId = book?.cover_edition_key || book.lending_edition_s
+      const authorId = book?.author_key 
+
+      return {
+        title: book.title,
+        coverId: coverId || null,
+        authorId: authorId || null,
+        author_name: book.author_name?.[0] || null,
+        first_publish_year: book.first_publish_year || null,
+        subjects: book.subject || [],
+        coverUrl: coverId
+          ? `https://covers.openlibrary.org/b/olid/${coverId}-L.jpg?default=false`
+          : null,
+        authorUrl: authorId
+        ? `https://covers.openlibrary.org/a/olid/${authorId}-L.jpg?default=false`
+        : null
+      }
+    })
+
     return res
     .status(200)
-    .json(new ApiResponse(200, newData.docs, 'data fetched successfully'))
+    .json(new ApiResponse(200, booksData, 'data fetched successfully'))
+
 })
 
 export { fetchDataFromAPI }
