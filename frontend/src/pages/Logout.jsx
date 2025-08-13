23366c51bd893c4662e9719a47c3cc9cb3c730cd
wiredbrat <vermaatul291002@gmail.com>
2025-08-13 23:54:16 +0530

    write auth cleanup on logout

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import callApi from '../utils/callApi'
 import userRoutes from '../routes/user.routes'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { AuthContext } from '../context/AuthContext'
 
 function Logout() {
   const navigate = useNavigate()
+  const { setAuthData } = useContext(AuthContext)
   useEffect(() => async() => {
     try {
       const logout = await axios.post('http://localhost:8000/api/v1/user/logout', {}, {withCredentials: true})
       
       if(logout.status === 200) {
         navigate('/discover')
+        setAuthData(null)
       }
     } catch (error) {
       console.log(error)
