0a0e1da19801f2c89a3e2d8bc70c835717d148b7
wiredbrat <vermaatul291002@gmail.com>
2025-08-17 15:19:53 +0530

    add ExploreBooks and User components to Importer export

--- frontend/src/Importer.jsx
+++ frontend/src/Importer.jsx
@@ -1,5 +1,7 @@
+import BookDetails from "./components/BookDetails";
 import Books from "./components/Books";
 import Card from "./components/Card";
+import ExploreBooks from "./components/exploreBooks";
 import LoginRegisterForm from "./components/LoginRegisterForm";
 import OtpForm from "./components/OtpForm";
 import Searchbar from "./components/searchbar";
@@ -8,5 +10,20 @@ import Category from "./pages/Category";
 import Content from "./pages/Content";
 import Library from "./pages/Library";
 import Logout from "./pages/Logout";
+import User from "./pages/User";
 
-export { OtpForm, LoginRegisterForm, Searchbar, Sidebar, Books, Card, Content, Library, Category, Logout}
+export { 
+  OtpForm, 
+  LoginRegisterForm, 
+  Searchbar, 
+  Sidebar, 
+  Books, 
+  Card, 
+  Content, 
+  Library, 
+  Category, 
+  Logout, 
+  BookDetails, 
+  User,
+  ExploreBooks
+}
