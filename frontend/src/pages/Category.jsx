032a050d24267089e70b68cda7be3a641dc2b125
wiredbrat <vermaatul291002@gmail.com>
2025-08-19 00:00:40 +0530

    update layout styles in Category component for better responsiveness

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -3,7 +3,7 @@ import { Books } from '../Importer'
 
 function Category() {
   return (
-    <div className='absolute w-full md:w-[79%] top-20 right-2 bottom-2 overflow-auto scrollbar-hide'>
+    <div className='absolute w-full md:w-[69%] lg:w-[79%] top-20 md:right-2 bottom-2 overflow-auto scrollbar-hide'>
       <div className={`grid grid-flow-row gap-4 h-auto `}>
         <Books category={'thriller'}/>
         <Books category={'romance'}/>
