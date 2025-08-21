4e23816f3da6e3c67bd151e51076af437aa1eb65
wiredbrat <vermaatul291002@gmail.com>
2025-08-21 22:32:27 +0530

    add Support component for user support section

---
+++ frontend/src/pages/Support.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Support() {
+  return (
+    <div className='absolute w-full md:w-[69%] lg:w-[79%] top-20 md:right-2 bottom-2 overflow-auto scrollbar-hide'>
+      <div className={`grid grid-flow-row gap-4 h-auto mx-5`}>
+        Support me
+      </div>
+    </div>
+  )
+}
+
+export default Support
