11401f7133a7ccf6241c5ab285b50bb38fcb2276
wiredbrat <vermaatul291002@gmail.com>
2025-08-25 22:03:46 +0530

    add favorite functionality to Card component with login redirect

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,18 +1,40 @@
-import {useContext} from 'react'
-import { ThemeContext } from '../context/ThemeContext'
 import { motion } from 'motion/react'
-import { Link } from 'react-router-dom'
+import { useContext, useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { VscHeart, VscHeartFilled  } from "react-icons/vsc";
+import { AuthContext } from '../context/AuthContext';
+import { BookContext } from '../context/BookContext';
+import axios from 'axios';
+import { handleCardClick } from '../utils/handleCardClick';
 
-function Card({key, imageUrl, bookName, bookId}) {
-  const {theme} = useContext(ThemeContext)
+
+function Card({imageUrl, bookName, bookId, workKey}) {
+  const navigate = useNavigate()
+  const [isFavorite, SetIsFavorite] = useState(false)
+  const { authData } = useContext(AuthContext )
+  const isLoggedIn = Boolean(authData && Object.keys(authData).length > 0);
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const {bookData, setBookData} = useContext(BookContext)
+  const handleFavorite = () => {
+    if(!isLoggedIn) {
+      navigate('/login')
+    }
+    SetIsFavorite(prev => !prev)  
+  } 
+
   return (
     <motion.div 
       initial={{opacity: 0, y: 20}}
       animate={{opacity:1, y: 0, transition: {duration: 0.3}}}
-      key={key} 
-      className={`card rounded-md px-3 py-4 m-2 shadow-md`}
-    >
-      <Link to={`https://archive.org/details/${bookId}`}>
+      key={workKey} 
+      className={`card relative rounded-md px-3 py-4 m-2 shadow-md w-40`}
+    > 
+      <span 
+        className={`${isFavorite ? 'isFavorite' : 'text-gray-200'} absolute cursor-pointer bottom-3 text-xl inline-block backdrop-blur-sm bg-[#5454541e] p-[6px] rounded-2xl`} 
+        onClick={handleFavorite}
+      >{isFavorite ? <VscHeartFilled /> : <VscHeart />}</span>
+      <Link onClick={() => handleCardClick(bookId, workKey, imageUrl, setBookData)}>
       <img 
         className='h-48 w-36 object-fill'
         src={imageUrl} 
