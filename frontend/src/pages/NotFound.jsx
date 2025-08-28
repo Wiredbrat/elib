6e6b363e03f06e610b19a0774791593591c707c8
wiredbrat <vermaatul291002@gmail.com>
2025-08-28 22:06:09 +0530

    add 404 page for undefined routes

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NotFound() {
+  return (
+    <div className='flex flex-col justify-center gap-2 items-center h-screen'>
+      <div className='text-color text-2xl'>404 </div>
+      <div className='text-color text-2xl font-bold'> Page Not Found</div>
+      <Link to='/discover' className='text-color-alt underline'> Back to Home</Link>
+    </div>
+  )
+}
+
+export default NotFound
