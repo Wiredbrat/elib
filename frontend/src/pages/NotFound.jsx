7cee876a7df6ff29cb13b4aeabc268847943bf02
wiredbrat <vermaatul291002@gmail.com>
2025-08-31 13:28:01 +0530

    fix NotFound component link styling for consistency

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ function NotFound() {
     <div className='flex flex-col justify-center gap-2 items-center h-screen'>
       <div className='text-color text-2xl'>404 </div>
       <div className='text-color text-2xl font-bold'> Page Not Found</div>
-      <Link to='/discover' className='text-color-alt underline'> Back to Home</Link>
+      <Link to='/discover' className='text-color-alt underline '> Back to Home</Link>
     </div>
   )
 }
