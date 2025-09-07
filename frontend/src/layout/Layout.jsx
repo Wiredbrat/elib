81892cb6b8c0c07386c02b00d65962f2dcc75768
wiredbrat <vermaatul291002@gmail.com>
2025-09-07 11:36:59 +0530

    add routes for book details, user, explore books, settings, and support in Layout component

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Outlet, Route } from 'react-router-dom'
-import { OtpForm, Content, Library, Category, Logout } from '../Importer'
+import { OtpForm, Content, Library, Category, Logout, BookDetails, User, ExploreBooks, Settings, Support, NotFound } from '../Importer'
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import { Suspense } from 'react';
 
 Login
 function Layout() {
@@ -17,13 +17,21 @@ function Layout() {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+
     <Route path='/' element={<Layout/>}>
+      
       <Route path='/discover' element={<Content/>}/>
       
       <Route path='/library' element={<Library/>}/>
       <Route path='/categories' element={<Category/>}/>
+      <Route path='/book/:bookId' element={<BookDetails/>}/>
+      <Route path='/user' element={<User/>}/>
+      <Route path='/categories/:category' element={<ExploreBooks/>}/>
+      <Route path='/user/settings' element={<Settings/>}/>
+      <Route path='/support' element={<Support/>}/>
+      
     </Route>
-    
+    <Route path='*' element={<NotFound/>}/>
     <Route path='/login' element={<Login/>}/>
     <Route path='/otp' element={<OtpForm/>}/>
     <Route path='/logout' element={<Logout/>}/>
