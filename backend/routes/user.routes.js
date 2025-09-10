ffc8c44ab60ae1604fdf9c269703d92ac57c406c
wiredbrat <vermaatul291002@gmail.com>
2025-09-10 22:07:23 +0530

    fix: add OTP reset password route with validation

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,7 @@
 import Router from 'express'
 import { userLogin, userRegister, userLogout, changePassword, getUserCollection, deleteUserAccount, getUser } from '../controllers/user.controller.js'
 import { validateToken } from '../middlewares/auth.middleware.js'
+import { isValidUser, sendOTP } from '../utils/otpValidation.js'
 
 
 const router = Router()
@@ -15,5 +16,6 @@ router.route('/change-password').post(validateToken, changePassword)
 router.route('/get-user/books').get(validateToken, getUserCollection)
 router.route('/delete-account').post(validateToken, deleteUserAccount)
 router.route('/get-user').get(validateToken, getUser)
+router.route('/reset/password').post(isValidUser, sendOTP)
 
 export { router }
